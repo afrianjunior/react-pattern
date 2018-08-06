bd345144ee14150d3d1c57e6e46e29340a7ff214
adiatma <adiatma9024@gmail.com>
2018-08-06 22:35:35 +0700

    MODIFIED: add delete method

--- src/TodoLists.jsx
+++ src/TodoLists.jsx
@@ -7,10 +7,16 @@ export default class TodoLists extends React.Component {
       todos: todos.concat(todo),
     }));
   }
+  deleteTodo = (todoIndex) => {
+    this.setState(({ todos }) => ({
+      todos: todos.filter((todo, index) => index !== todoIndex)
+    }));
+  }
   render() {
     return this.props.children({
       todos: this.state.todos,
       addTodo: this.addTodo,
+      deleteTodo: this.deleteTodo,
     })
   }
 }
