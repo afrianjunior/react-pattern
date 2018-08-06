ff46fd045df5a654a32e6d784044dba9cb68ad03
afrianjunior <afrian.junior26@gmail.com>
2018-08-06 23:05:53 +0700

    MODIFIED: add input styled component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
 import React from "react";
 import TodoLists from "./TodoLists";
+import styled from "styled-components";
+
+const Input = styled.input`
+  padding: .5em 1.3em;
+  border-radius: 4px;
+  outline: none;
+  background-color: grey;
+  color: white;
+  border: 0;
+  margin-right: 10px;
+`;
 
 export default class App extends React.Component {
   render() {
@@ -25,7 +36,7 @@ export default class App extends React.Component {
                   todo.value = "";
                 }}
               >
-                <input type="text" name="todo" placeholder="todo" />
+                <Input type="text" name="todo" placeholder="todo" />
                 <input type="submit" value="Add" />
               </form>
             </React.Fragment>
